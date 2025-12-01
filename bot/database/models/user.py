dd3d8d8c15915850b0e5ba68a0fcdf0ea9e79ed4
F1zzTao <f1zztao@proton.me>
2025-12-01 18:34:13 +0200

    fix: Set default model id to 1 (GLM)

--- bot/database/models/user.py
+++ bot/database/models/user.py
@@ -15,7 +15,7 @@ class UserModel(Base):
     platform: Mapped[str]
 
     current_mood_id: Mapped[int] = mapped_column(default=0)
-    current_model_id: Mapped[Optional[str]] = mapped_column(default="18")
+    current_model_id: Mapped[Optional[str]] = mapped_column(default="1")
 
     persona: Mapped[str] = mapped_column(default="")
     created_moods: Mapped[List["MoodModel"]] = relationship("MoodModel")
